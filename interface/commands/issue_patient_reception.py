e655e18f8218f99d77f8214cb04d359c1dde0656
Ilya Medyanikov <ilya.medyanikov@gmail.com>
2019-06-04 19:08:27 +0300

    Add Issue patient reception

--- interface/commands/issue_patient_reception.py
+++ interface/commands/issue_patient_reception.py
@@ -1,11 +1,27 @@
 import tkinter
 
 
-from interface.common import tkinter_common
+from interface.commands.patients_selector import PatientsSelector
 
 
-class IssuePatientReception(tkinter.Toplevel):
+class IssuePatientReception:
     def __init__(self, parent, controller):
-        super().__init__(parent)
-        tkinter_common.default_init(self, parent, controller)
-        tkinter_common.center_window(self)
+        data = (
+            ('Doctor Type', 'str'),
+            ('Date', 'date'),
+            ('Verdict', 'str')
+        )
+
+        def callback(patient, data):
+            if not 'Diseases' in patient:
+                patient['Diseases'] = [{}]
+            if 'Wrote out' in patient['Diseases'][-1]:
+                patient['Diseases'].append({})
+            if not 'Receptions' in patient['Diseases'][-1]:
+                patient['Diseases'][-1]['Receptions'] = []
+            data['Doctor Name'] = controller.get_user_name()
+            patient['Diseases'][-1]['Receptions'].append(data)
+            controller.update_patient(patient)
+
+        PatientsSelector(parent, controller, data, 'Issue Patient Reception', callback, False)
+
